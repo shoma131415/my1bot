8a772c1cbe8bc292e4fd2925316c39cd37b32486
shoma131415 <shamillote@gmail.com>
2024-10-15 13:47:37 +0300

    Create my1bot.py

---
+++ my1bot.py
@@ -0,0 +1,21 @@
+import telebot
+
+bot = telebot.TeleBot("")
+ # Обработчик команды '/start' и '/hello'
+@bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    bot.reply_to(message, f'Привет! Я бот {bot.get_me().first_name}!')
+    
+    # Обработчик команды '/heh'
+@bot.message_handler(commands=['heh'])
+def send_heh(message):
+    count_heh = int(message.text.split()[1]) if len(message.text.split()) > 1 else 5
+    bot.reply_to(message, "he" * count_heh)
+
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+    # Запуск бота
+bot.polling()
